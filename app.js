0b823b61015973d72138312cd5c423e0f6315e22
y.terui <sakulovesmileee@gmail.com>
2023-12-13 14:24:13 +0900

    ユーザー情報システムからIDで情報の取得

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ app.get('/', (req, res) => {
 app.use(express.static('assets'));
   const sql = "select * from users";
   // 参考例
-  const num = 10000;
+  // const num = 10000;
   // 基礎課題
   /* ==========従来通りJavaScriptの要領で書いてください。==========
     ここで記載する内容はブラウザに出力するための情報のみになります。上記参考例のconst num = 10000;のように
     各基礎課題で指定された情報を一つの変数に格納していきましょう。各情報を変数に格納したら今度は下にある
     コメントアウト⓵の部分を確認してみて下さい。*/
     // 基礎課題01:文字列を画面に出力しましょう。
-  const hundred = 100;
+  // const hundred = 100;
     // 基礎課題02:リストを画面表示
     // app.jsのここで配列を用意し、viewsフォルダのindex.ejsのscriptタグ内で画面に出力出来るように機能を作成して下さい。
-  const array = ['いちご', 'みかん', 'もも'];
+  // const array = ['いちご', 'みかん', 'もも'];
   //   基礎課題03:マップを画面表示
   //   マップというのは配列の中にオブジェクトを設定するものになります。よく分からない方は
   //   オブジェクトを以下のように設定
@@ -42,13 +42,13 @@ app.use(express.static('assets'));
     // name: 't.suzuki', email: 't.suzuki@gmail.com'
     // name: 't.hasegawa', email: 't.hasegawa@gmail.com'
   //
-  const map = [
-    {name: 's.chiba', email: 's.chiba@gmail.com'},
-    {name: 't.kosuge', email: 't.kosuge@gmail.com'},
-    {name: 'm.chiba', email: 'm.chiba@gmail.com'},
-    {name: 't.suzuki', email: 't.suzuki@gmail.com'},
-    {name: 't.hasegawa', email: 't.hasegawa@gmail.com'}
-  ];
+  // const map = [
+  //   {name: 's.chiba', email: 's.chiba@gmail.com'},
+  //   {name: 't.kosuge', email: 't.kosuge@gmail.com'},
+  //   {name: 'm.chiba', email: 'm.chiba@gmail.com'},
+  //   {name: 't.suzuki', email: 't.suzuki@gmail.com'},
+  //   {name: 't.hasegawa', email: 't.hasegawa@gmail.com'}
+  // ];
   // 空のオブジェクト、nameとemailを5セット 配列の何番目のどのオブジェクト
   // ==========ここまでの範囲で書くようにしましょう。==========
   con.query(sql, function (err, result, fields) {
@@ -60,10 +60,10 @@ app.use(express.static('assets'));
         指定の仕方はオブジェクトの考え方と同じで、プロパティ名: 値の形になります。値の部分は変数名を入れるようにして下さい。
         プロパティ名はindex.ejsで使用しますので、何の値が入ってるかわかるような名前にしましょう。
       */
-      number: num,
-      hundred: hundred,
-      array: array,
-      map: map,
+      // number: num,
+      // hundred: hundred,
+      // array: array,
+      // map: map,
     });
   });
 });
